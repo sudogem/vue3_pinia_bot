9c8254dccf3ea363c7ca05c108548d25e0526eec
sudogem <sudogem@gmail.com>
2024-03-13 00:45:34 +0800

    Defining Getters in an Options Store

--- src/stores/cart-options.js
+++ src/stores/cart-options.js
@@ -1,7 +1,31 @@
 import { defineStore } from "pinia"
+import { useProductStore } from "./product"
+
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
     productIds: [],
   }),
+  getters: {
+    // In order to get access to productIds state inside our getter we use the state parameter.
+    // This is how you add a getter and how to access another store from inside a getter.
+    // Just note that you will need to call the useProductStore call in every getter/action or
+    //  inside the state function anywhere where you need to access data from another store.
+    //  So you may end up calling your use...Store methods multiple times inside a single store.
+    cart: (state) => {
+      const productStore = useProductStore()
+
+      return state.productIds.map(id => productStore.products.find(p => p.id === id))
+    },
+    // Note: Getter functions like cart are not part of this state parameter.
+    // So we can't access this cart getter from this state parameter.
+    // cartTotal: (state) => {
+    //   return state.cart.value.reduce((prev, cur) => prev += cur.price, 0)
+    // },
+    // In order to access the getter cart functions is to define the cartTotal as a
+    //   regular function unlike the cart function it is define as a property not a function.
+    cartTotal() {
+      return this.cart.value.reduce((prev, cur) => prev += cur.price, 0)
+    },
+  },
 })
